fbd58f586d9cf6438935509a8020dedcdfd1b87f
Anthony Giuntini <giuntini1996@gmail.com>
2020-08-04 21:47:57 -0400

    Added recording from week 1

---
+++ Notes/Week 1/Shell output week 1.py
@@ -0,0 +1,175 @@
+Python 3.8.5 (v3.8.5:580fbb018f, Jul 20 2020, 12:11:27) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+>>> print("Hello world")
+Hello world
+>>> 1
+1
+>>> 1.0
+1.0
+>>> 1+1+
+SyntaxError: invalid syntax
+>>> 1+1
+2
+>>> 2*2
+4
+>>> 4/2
+2.0
+>>> 1.0+1
+2.0
+>>> type(2)
+<class 'int'>
+>>> type(print)
+<class 'builtin_function_or_method'>
+>>> 2+2*4
+10
+>>> round(10.999999, 2)
+11.0
+>>> round(10.54345, 3)
+10.543
+>>> "Anthony"
+'Anthony'
+>>> anthony
+Traceback (most recent call last):
+  File "<pyshell#15>", line 1, in <module>
+    anthony
+NameError: name 'anthony' is not defined
+>>> 'Anthony'
+'Anthony'
+>>> '''Anthony'''
+'Anthony'
+>>> "anthony
+SyntaxError: EOL while scanning string literal
+>>> '''
+Anthony
+Giuntini
+'''
+'\nAnthony\nGiuntini\n'
+>>> 'Mather's
+SyntaxError: invalid syntax
+>>> 'Mather\'s'
+"Mather's"
+>>> '"Hello"'
+'"Hello"'
+>>> print("'Hello'")
+'Hello'
+>>> box_a = "This is a variable"
+>>> box_a
+'This is a variable'
+>>> s = ':)'
+>>> s*2
+':):)'
+>>> 'appl' + 'e'
+'apple'
+>>> 'a' *5
+'aaaaa'
+>>> digit1 = 1
+>>> digit2 = 2
+>>> digit1 * digit2
+2
+>>> digit1 + digit2
+3
+>>> 1a = 1
+SyntaxError: invalid syntax
+>>> a_b = 2
+>>> a-b = 2
+SyntaxError: cannot assign to operator
+>>> num1 = 1
+>>> num2 = 2
+>>> A = 1
+>>> a = 1
+>>> a = 1
+>>> a = 2
+>>> a
+2
+>>> A
+1
+>>> digit1
+1
+>>> 
+= RESTART: /Users/agiuntini/Documents/Schuyler/Python Seminar/Class Examples/Class 1/First program.py
+>>> 
+= RESTART: /Users/agiuntini/Documents/Schuyler/Python Seminar/Class Examples/Class 1/First program.py
+Hello World
+>>> 
+= RESTART: /Users/agiuntini/Documents/Schuyler/Python Seminar/Class Examples/Class 1/First program.py
+1
+>>> 
+= RESTART: /Users/agiuntini/Documents/Schuyler/Python Seminar/Class Examples/Class 1/First program.py
+Traceback (most recent call last):
+  File "/Users/agiuntini/Documents/Schuyler/Python Seminar/Class Examples/Class 1/First program.py", line 2, in <module>
+    print('This number is: ' + 1)
+TypeError: can only concatenate str (not "int") to str
+>>> 
+= RESTART: /Users/agiuntini/Documents/Schuyler/Python Seminar/Class Examples/Class 1/First program.py
+This number is: 1
+>>> 
+= RESTART: /Users/agiuntini/Documents/Schuyler/Python Seminar/Class Examples/Class 1/First program.py
+= RESTART: /Users/agiuntini/Documents/Schuyler/Python Seminar/Class Examples/Class 1/First program.py
+This number is: 1
+>>> 
+= RESTART: /Users/agiuntini/Documents/Schuyler/Python Seminar/Class Examples/Class 1/First program.py
+= RESTART: /Users/agiuntini/Documents/Schuyler/Python Seminar/Class Examples/Class 1/First program.pyHello World
+= RESTART: /Users/agiuntini/Documents/Schuyler/Python Seminar/Class Examples/Class 1/First program.pyHello World
+>>> 
+= RESTART: /Users/agiuntini/Documents/Schuyler/Python Seminar/Class Examples/Class 1/First program.py
+What is your name: Anthony
+Anthony
+>>> 
+= RESTART: /Users/agiuntini/Documents/Schuyler/Python Seminar/Class Examples/Class 1/First program.py
+What is your name: Daniel
+Welcome, Daniel
+>>> True
+True
+>>> False
+False
+>>> type(True)
+<class 'bool'>
+>>> True == True
+True
+>>> True == False
+False
+>>> True != False
+True
+>>> 1 == 0
+False
+>>> 1 == 1
+True
+>>> 1 == True
+True
+>>> 1 == 0
+False
+>>> 0 == False
+True
+>>> 1 > 0
+True
+>>> 10 < 11
+True
+>>> 11 >= 11
+True
+>>> 
+= RESTART: /Users/agiuntini/Documents/Schuyler/Python Seminar/Class Examples/Class 1/First program.py
+Do this
+This was true
+>>> 1 == "1"
+False
+>>> 
+= RESTART: /Users/agiuntini/Documents/Schuyler/Python Seminar/Class Examples/Class 1/First program.py
+Enter a number: 1
+Do this
+This was true
+>>> 
+= RESTART: /Users/agiuntini/Documents/Schuyler/Python Seminar/Class Examples/Class 1/First program.py
+Enter a number: 10
+>>> 
+= RESTART: /Users/agiuntini/Documents/Schuyler/Python Seminar/Class Examples/Class 1/First program.py
+Enter a number: 10
+>>> 
+= RESTART: /Users/agiuntini/Documents/Schuyler/Python Seminar/Class Examples/Class 1/First program.py
+Say Hi: hi
+You said hi
+>>> 
+= RESTART: /Users/agiuntini/Documents/Schuyler/Python Seminar/Class Examples/Class 1/First program.py
+Say Hi: Hi
+>>> 
